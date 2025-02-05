8bce6811f82fdafd5d4b0357a7290c9a8fb1a471
Helena Simonsen <hesi016@student.kristiania.no>
2025-02-05 14:25:53 +0100

    show more layers(not working yet)

---
+++ src/app/application.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef } from "react";
+import { Map, View } from "ol";
+import TileLayer from "ol/layer/Tile";
+import { OSM } from "ol/source";
+import { useGeographic } from "ol/proj";
+
+// Styling of OpenLayers components like zoom and pan controls
+import "ol/ol.css";
+import VectorLayer from "ol/layer/Vector";
+import VectorSource from "ol/source/Vector";
+import { GeoJSON } from "ol/format";
+
+useGeographic();
+
+const osmLayer = new TileLayer({ source: new OSM() });
+const municipalityLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/kommuneer.geojson",
+    format: new GeoJSON(),
+  }),
+});
+
+const schoolLayer = new VectorLayer({
+  source: new VectorSource({
+    url: "/geojson/skoler.geojson",
+    format: new GeoJSON(),
+  }),
+});
+
+const map = new Map({
+  view: new View({ center: [10.8, 59.9], zoom: 13 }),
+  layers: [osmLayer, municipalityLayer, schoolLayer],
+});
+
+export function Application() {
+  const mapRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    map.setTarget(mapRef.current!);
+  }, []);
+
+  return <div ref={mapRef}></div>;
+}
