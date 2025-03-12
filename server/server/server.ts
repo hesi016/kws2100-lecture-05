07642536b58e9be812edabd7e13dd6a45a4368e0
Helena Simonsen <hesi016@student.kristiania.no>
2025-03-12 15:21:29 +0100

    Updating server

--- server/server/server.ts
+++ server/server/server.ts
@@ -1,38 +1,7 @@
 import { Hono } from "hono";
 import { serve } from "@hono/node-server";
-import pg from "pg";
-import { serveStatic } from "@hono/node-server/serve-static";
-
-// For Heroku
-const connectionString = process.env.DATABASE_URL;
-
-const postgresql = connectionString
-  ? new pg.Pool({ connectionString, ssl: { rejectUnauthorized: false } })
-  : new pg.Pool({ user: "postgres" });
-
 const app = new Hono();
-app.get("/api/kommuner", async (c) => {
-  const result = await postgresql.query(
-    "select kommunenummer, kommunenavn, st_transform(st_simplify(omrade, 100), 4326)::json as geometry from kommune",
-  );
-  return c.json({
-    type: "FeatureCollection",
-    crs: {
-      type: "name",
-      properties: {
-        name: "urn:ogc:def:crs:OGC:1.3:CRS84",
-      },
-    },
-    features: result.rows.map(
-      ({ kommunenummer, kommunenavn, geometry: { coordinates } }) => ({
-        type: "Feature",
-        geometry: { type: "MultiPolygon", coordinates },
-        properties: { kommunenummer, kommunenavn },
-      }),
-    ),
-  });
+app.get("/api/hello", async (c) => {
+  return c.text("Hello World!");
 });
-app.use("*", serveStatic({ root: "../dist" }));
-
-const port = process.env.PORT ? parseInt(process.env.PORT) : 3000;
-serve({ fetch: app.fetch, port });
+serve(app);
